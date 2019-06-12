90a6bb1101996a590edffac110c6aae20cd5f2fe
Ray Santana <santanaray6@gmail.com>
2019-06-12 01:22:47 -0500

    Rewrote some parts of the code

--- src/Server.java
+++ src/Server.java
@@ -5,126 +5,166 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+/*
+   W [B|R]        -  Welcome message with assigned color.
+   V [Y|N] [pos]  -  Validation message with position and extra turn.
+   O [Y|N] [pos]  -  Opponent's move with position and extra turn
+   E [B|R|T]      -  End game message with winner color (or T for tie).
+   I [message]    -  Information sent by server for client to display.
+   M [pos]        -  Move made by player; message sent by client.
+   S [B|R] [pos]  -  Square completed; include position and player color.
+   Q [B|R]        -  Quit message (with player color) sent by client.
+*/
 
-    public static void main(String[] args) throws Exception {
-        ServerSocket listener = new ServerSocket(8901);
-        System.out.println("Server is running...");
+public class Server {
+    private static final int PORT = 8901;
+
+    public static void main(String[] args) {
+        /* Open the server socket and wait for players to connect. Assign the
+           first player to color blue and second player to the color red. */
+        ServerSocket listener = null;      
         try {
+            listener = new ServerSocket(PORT);
+            System.out.println("Server has started...");
             while (true) {
-                /* Start the game and wait for players to connect. Assign the first
-                   player to the color blue and second player to the color red. */
                 Game game = new Game();
                 Game.Player player1 = game.new Player(listener.accept(), 'B');
                 Game.Player player2 = game.new Player(listener.accept(), 'R');
                 player1.setOpponent(player2);
                 player2.setOpponent(player1);
-                game.currentPlayer = player1;
+                game.currentPlayer = player1; /* Player 1 goes first */
                 player1.start();
                 player2.start();
             }
+        } catch (Exception error) {
+            error.printStackTrace();
         } finally {
-            listener.close();
-        }
+            try {
+                listener.close();
+            } catch (IOException e) {};
+        } 
     }
 }
 
 class Game {
-
-    private int gridWidth = 9; //must be odd number greater than 3
-    private Player[] board = new Player[gridWidth*gridWidth];
+    private static final int GRID_LENGTH = 7;
+    private char[] board = new char[GRID_LENGTH*GRID_LENGTH];
+    private int bluePoints = 0;
+    private int redPoints = 0;
     Player currentPlayer;
-    private boolean goAgain;
-    private int Squares;
-    private int bluePoints;
-    private int redPoints;
 
-    public String gameWinner() {
-        // Find out which player has the most points
+    public char gameWinner() {
+        /* Find out which player has the most points. */
         System.out.println("Blue: " + bluePoints + " points");
         System.out.println("Red: " + redPoints + " points");
-        if (bluePoints == redPoints) { return "TIE"; }
-        else { return (bluePoints > redPoints) ? "BLUE" : "RED"; }
+        if (bluePoints == redPoints) { 
+            return 'T';
+        } else { 
+            return (bluePoints > redPoints) ? 'B' : 'R';
+        }
     }
 
-    public int checkSquares() {
-        // Count the number of boxes completed so far
-        int s = 0;
+    public int countCompletedSquares(int pos, Player player) {
+        /* Count the number grid squares that have been completed. */
+        int row = pos / GRID_LENGTH;
+        int numCompletedSquares = 0;
 
-        int m = (gridWidth-1)/2;
-        int i = 1;
-        for (int k = 1; k < m*m; k++) {
-            if (board[i] != null && board[i+gridWidth-1] != null && board[i+gridWidth+1] != null
-                    && board[i+2*gridWidth] != null) {
-                s++;
+        if (row % 2 == 0) {
+            /* The player chose a horizontal grid line. */
+            if (row != 0) {
+                /* Check if the grid square above is complete. */
+                if (board[pos-2*GRID_LENGTH] != 0
+                 && board[pos-GRID_LENGTH+1] != 0
+                 && board[pos-GRID_LENGTH-1] != 0) {
+                    ++numCompletedSquares;
+                    player.squareCompleted(pos-GRID_LENGTH);
+                }
+            } 
+            if (row != (GRID_LENGTH-1)) {
+                /* Check if the grid square below is complete. */
+                if (board[pos+2*GRID_LENGTH] != 0
+                 && board[pos+GRID_LENGTH+1] != 0
+                 && board[pos+GRID_LENGTH-1] != 0) {
+                    ++numCompletedSquares;
+                    player.squareCompleted(pos+GRID_LENGTH);
+                }
+            } 
+        } else {
+            /* The player chose a vertical grid line. */
+            int col = pos % GRID_LENGTH;
+
+            if (col != 0) {
+                /* Check if the grid square on the left is complete. */
+                if (board[pos-2] != 0
+                 && board[pos-GRID_LENGTH-1] != 0
+                 && board[pos+GRID_LENGTH-1] != 0) {
+                    ++numCompletedSquares;
+                    player.squareCompleted(pos-1);
+                }
             }
-            i = (k % m == 0) ? (i + gridWidth + 3) : (i + 2);
+            if (col != (GRID_LENGTH-1)) {
+                /* Check if the grid square on the right is complete. */
+                if (board[pos+2] != 0
+                 && board[pos+GRID_LENGTH+1] != 0
+                 && board[pos-GRID_LENGTH+1] != 0) {
+                     ++numCompletedSquares;
+                     player.squareCompleted(pos+1);
+                 }
+            }
         }
 
-        return s;
-    }
-
-    public boolean completedSquare() {
-        // Return true if the current player completes a box
-        int n = checkSquares();
-        if (n > Squares) {
-            Squares = n;
-            return true;
-        }
-        return false;
+        return numCompletedSquares;
     }
 
     public boolean boardFilledUp() {
-        // Game ends when the board is filled up
-        for (int i = 0; i < ((board.length-1)/2); i++) {
-            if (board[2*i+1] == null) {
-                return false;
-            }
-        }
-        return true;
+        /* Check if grid is complete and game should end. */
+        int totalPossibleSquares = (GRID_LENGTH / 2) * (GRID_LENGTH / 2);
+        return (totalPossibleSquares == (bluePoints + redPoints));
     }
 
-    public synchronized boolean legalMove(int location, Player player) {
-        // Determine if the move made by the player is allowed
-        if (player == currentPlayer && board[location] == null) {
-            board[location] = currentPlayer;
-            if (completedSquare()) {
-                // Give the player points when they complete boxes and let them have an extra turn
-                int x = Squares - (bluePoints + redPoints);
-                if (currentPlayer.mark == 'B') { bluePoints += x; }
-                else { redPoints += x; }
-                goAgain = true;
-                currentPlayer.opponent.otherPlayerMoved(location);
-            }
-            else {
-                // End the current player's turn
-                goAgain = false;
+    public synchronized boolean legalMove(int position, Player player) {
+        /* Determine if the move made by the player is allowed. */
+        if ((player == currentPlayer) && (board[position] == 0)) {
+            board[position] = currentPlayer.playerColor;
+            
+            int numSquares = countCompletedSquares(position, player);
+            if (numSquares > 0) {
+                currentPlayer.thisPlayerMoved(position, true);
+                if (player.playerColor == 'B') {
+                    bluePoints += numSquares;
+                } else {
+                    redPoints += numSquares;
+                }
+            } else {
+                currentPlayer.thisPlayerMoved(position, false);
                 currentPlayer = currentPlayer.opponent;
-                currentPlayer.otherPlayerMoved(location);
             }
+
             return true;
         }
+
         return false;
     }
 
     class Player extends Thread {
-        char mark;
-        Player opponent;
-        Socket socket;
-        BufferedReader input;
-        PrintWriter output;
+        private BufferedReader input;
+        private PrintWriter output;
+        private Socket socket;
+        private Player opponent;
+        private char playerColor;
 
-        public Player(Socket socket, char mark) {
+        public Player(Socket socket, char pColor) {
             this.socket = socket;
-            this.mark = mark;
+            this.playerColor = pColor;
             try {
                 input = new BufferedReader(
                         new InputStreamReader(socket.getInputStream()));
                 output = new PrintWriter(socket.getOutputStream(), true);
-                output.println("WELCOME " + mark);
-                output.println("MESSAGE Waiting for opponent to connect");
+                output.println("W " + playerColor);
+                output.println("I Waiting for opponent to connect...");
             } catch (IOException e) {
-                System.out.println("Player died: " + e);
+                System.out.println("Couldn't connect to player :(");
+                e.printStackTrace();
             }
         }
 
@@ -132,42 +172,54 @@ class Game {
             this.opponent = opponent;
         }
 
-        public void otherPlayerMoved(int location) {
+        public void thisPlayerMoved(int position, boolean goAgain) {
             String again = goAgain ? "Y" : "N";
-            output.println("OPPONENT_MOVED " + again + location);
-            output.println(boardFilledUp() ? gameWinner() : "");
+            output.println("V " + again + " " + position);
+            opponent.output.println("O " + again + " " + position);
         }
 
-        public void run() {
-            Squares = 0;
-            bluePoints = 0;
-            redPoints = 0;
-            goAgain = false;
-            try {
-                output.println("MESSAGE All players connected");
+        public void squareCompleted(int position) {
+            String message = "S " + playerColor + " " + position;
+            output.println(message);
+            opponent.output.println(message);
+        }
 
-                if (mark == 'B') {
-                    output.println("MESSAGE Your move");
+        @Override
+        public void run() {
+            String command;
+            try {
+                /* Inform clients that both players have connected.
+                   Blue goes first. */
+                output.println("I All players connected");
+                if (playerColor == 'B') {
+                    output.println("I Your move");
                 }
 
                 while (true) {
-                    String command = input.readLine();
-                    if (command.startsWith("MOVE")) {
-                        int location = Integer.parseInt(command.substring(5));
-                        if (legalMove(location, this)) {
-                            output.println(goAgain ? "VALID_MOVE Y" : "VALID_MOVE N");
-                            output.println(boardFilledUp() ? gameWinner() : "");
+                    command = input.readLine();
+                    command = command.isEmpty() ? " " : command;
+                    if (command.charAt(0) == 'M') {
+                        int position = Integer.parseInt(command.substring(2));
+                        if (legalMove(position, this)) {
+                            if (boardFilledUp()) {
+                                char winner = gameWinner();
+                                output.println("E " + winner);
+                                opponent.output.println("E " + winner);
+                            }
                         } else {
-                            output.println("MESSAGE ?");
-                        }
-                    } else if (command.startsWith("QUIT")) {
-                        return;
+                            output.println("I Invalid move");
+                        }                        
+                    } else if (command.charAt(0) == 'Q') {
+                        /* Player has quit the game. */
+                        break;
                     }
                 }
-            } catch (IOException e) {
-                System.out.println("Player disconnected: " + e);
+            } catch (Exception e) {
+                System.out.println("Player disconnected :(");
             } finally {
-                try {socket.close();} catch (IOException e) {}
+                try {
+                    socket.close();
+                } catch (IOException e) {}
             }
         }
     }
